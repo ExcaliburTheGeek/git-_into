ccb7be85bd4285a3351a4a27eaeb7c7c646e6723
Aryanvm <vmaryan298@gmail.com>
2020-05-11 14:58:33 +0530

    tis commit has builded a horizontal chess board

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 canvas=document.getElementById("mycanvas");
-H=700;
+H=800;
 W=1200;
 
 canvas.height=H;
 canvas.width=W;
 
-pen=canvas.getContext('2d')
+pen=canvas.getContext('2d');
+
+let h=0,w=0;
+while(h<H)
+{
+    if(w<W)
+    {
+      if((h/100)%2==0)
+      pen.fillRect(w,h,100,100);
+      else 
+      pen.fillRect(w+100,h,100,100);
+      w+=200;
+    }
+    else{
+        h+=100;
+        w=0;
+    }
+}
+
